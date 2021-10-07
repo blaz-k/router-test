917a05b94ebaffa40b1bae036f626e55305d0239
blaz <blazyy@gmail.com>
2021-10-07 11:19:07 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+import Home from '../pages/Home.vue'
+
+
+const routes = [
+  { path: '/', name:'Home' , component: Home },
+  { path: '/about', name: 'About',
+  component: () =>
+  import('../pages/About.vue')}
+]
+
+const router = createRouter({
+ history: createWebHashHistory(process.env.BASE_URL),
+ routes
+})
+
+export default router
